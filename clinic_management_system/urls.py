3f382f337e30a519fca395f451ea39712a474635
rahimi-mohd <reimy94@gmail.com>
2024-09-23 19:54:12 +0800

    user handling, login and logout functioality

--- clinic_management_system/urls.py
+++ clinic_management_system/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("clinic.urls")),
-    # path("", include("accounts.urls")),
+    path("", include("accounts.urls")),
 ]
