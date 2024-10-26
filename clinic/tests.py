5fb67246b4e8ceb8c7208f5f652943c347a02915
rahimi-mohd <reimy94@gmail.com>
2024-10-26 21:02:28 +0800

    add test

--- clinic/tests.py
+++ clinic/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+# class HomePageTest(TestCase):
+#     def test_home_page_returns_correct_html(self):
+#         response = self.client.get("/")
+#         self.assertContains(response, "<title>Home</title>")
+
+class LoginPageTest(TestCase):
+    def test_login_page(self):
+        response = self.client.get("/login")
+        self.assertContains(response, "<title>CMS</title>")
