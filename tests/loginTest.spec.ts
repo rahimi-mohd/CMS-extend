56ea93fa060fa72a1d1f788cd0b71040a689c7ec
rahimi <reimy94@gmail.com>
2025-01-19 12:48:50 +0800

    refactor codes refactor loginTest to accept user params instead of specific user refactor playwright.yml to add users data the same as loginTest.

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -1,30 +1,41 @@
 import { test, expect } from '@playwright/test';
 
-const admin = {
-    username: "admin",
-    password: "12",
-};
+const users = [
+    {
+        username: "admin",
+        password: "12",
+        roleName: "Admin",
+    },
+    {
+        username: "clinic_staff",
+        password: "bingo123#@!",
+        roleName: "Clinic Staff",
+    }
+]
 
 test.describe("Test login page", () => {
     test.beforeEach(async ({ page }) => {
         await page.goto("/");
-    })
-    test('test admin account', async ({ page }) => {
-
-        // make sure the login page element is visible
-        await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
-        await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
-
-        // fill in the username and password input
-        await page.getByLabel('Username*').fill(admin.username);
-        await page.getByLabel('Password*').fill(admin.password);
-
-        // click the login button
-        await page.getByRole('button', { name: 'Login' }).click();
-
-        // make sure the dashboard is visible
-        await expect(page.getByRole('heading', { name: 'Hello, Admin' })).toBeVisible();
-        await expect(page.getByRole('heading', { name: 'Clinic Management System' })).toBeVisible();
     });
+    users.forEach((user) => {
+        test(`test ${user.roleName} account`, async ({ page }) => {
+
+            // make sure the login page element is visible
+            await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+
+            // fill in the username and password input
+            await page.getByLabel('Username*').fill(user.username);
+            await page.getByLabel('Password*').fill(user.password);
+
+            // click the login button
+            await page.getByRole('button', { name: 'Login' }).click();
+
+            // make sure the dashboard is visible
+            await expect(page.getByRole('heading', { name: `Hello, ${user.username}` })).toBeVisible();
+            await expect(page.getByRole('heading', { name: 'Clinic Management System' })).toBeVisible();
+        });
+
+    })
 
 })
