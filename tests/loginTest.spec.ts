6d414ce7b2de41a419c22fe5d6062b204eeb035f
rahimi <reimy94@gmail.com>
2025-01-19 16:21:19 +0800

    add new test for all type of users and non-existing user

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -1,24 +1,35 @@
 import { test, expect } from '@playwright/test';
 
 const users = [
+  // admin
   {
     username: "admin",
     password: "12",
-    roleName: "Admin",
   },
+  // clinic staff
   {
-    username: "clinic_staff",
+    username: "sya",
     password: "bingo123#@!",
-    roleName: "Clinic Staff",
+  },
+  // doctor
+  {
+    username: "aira_aman",
+    password: "bingo123#@!",
   }
 ]
+
+// fake user
+const fakeUser = {
+  username: "fake",
+  password: "fakePassword"
+};
 
 test.describe("Test login page", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
   });
   users.forEach((user) => {
-    test(`test ${user.roleName} account`, async ({ page }) => {
+    test(`test ${user.username} account`, async ({ page }) => {
 
       // make sure the login page element is visible
       await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
@@ -37,6 +48,21 @@ test.describe("Test login page", () => {
       await expect(page.getByRole('link', { name: 'Appointment List' })).toBeVisible();
       await expect(page.getByRole('heading', { name: 'Clinic Management System' })).toBeVisible();
     });
+  });
+  test("test for not-existed account", async ({ page }) => {
+    // make sure the login page element is visible
+    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+
+    // fill in the username and password input
+    await page.getByLabel('Username*').fill(fakeUser.username);
+    await page.getByLabel('Password*').fill(fakeUser.password);
+
+    // click the login button
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // make sure the alert work
+    await expect(page.getByText('Please enter a correct')).toBeVisible();
 
   })
 
